c52653d4596405a3850ffa267c62513516e738ec
Keerthi-M <keermano2017@gmail.com>
2020-09-04 22:34:55 +0530

    exports keyword is used to make functions accessible outside of this module.

--- Modules/checkPassword.js
+++ Modules/checkPassword.js
@@ -1,10 +1,10 @@
 // "exports" keyword is used to make the variables and functions accessible from outside the current module.
-// A function named "Password" is defined with two arguments "username" and "password" along with "exports" keyword to make accessible of this file in someother module.
-exports.Password = function (username , password)
+// A function named "Password" is defined with two arguments "username" and "password" and "exports" keyword is used to make accessible of this file in someother module.
+exports.Password = function (Username , password)
 {
 
 // AND operator is used and this code will execute if one and only both the conditions defined are true.
-    if(username != "M.Keerthi" && password != "dt09102000")
+    if(Username != "M.Keerthi" && password != "dt09102000")
    {
        return "Sorry,Your password is incorrect. Please try again later :) ";
    }
