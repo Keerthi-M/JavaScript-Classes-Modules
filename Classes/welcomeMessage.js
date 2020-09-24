5261c719cf2048a52169d5eb1bb3d2d15fc8b1bc
Keerthi-M <keermano2017@gmail.com>
2020-09-24 20:00:12 +0530

    Objects and Method are used to access the user input which is get from the readline method

--- Classes/welcomeMessage.js
+++ Classes/welcomeMessage.js
@@ -7,30 +7,34 @@ Conditions :
 2.Input must be obtained through readline() method.
 3.UserName must be stored in dynamic. */
 
-// Variable named "community" is declared globally.
-var community;
-
-// A class is a type of function, but instead of using the keyword function to initiate it, we use the keyword class, and the properties are assigned inside a constructor() method.
+// A class is a type of function, but instead of using the keyword function to initiate it, we use the keyword class.
 // Class named "CySo" is defined using "class" keyword.
 class CySo { 
 
-// Passing user input "name" inside the method "Welcome"
-   Welcome (name) {
+// Method named "Welcome" is declared and user input is passing through this method.
+   Welcome(username) {
 
-// "this" keyword refers to the object "community" that belongs to the method "Welcome".
-    this.community = Welcome;
+// "this" keyword refers to the object "community".
+    this.community = username;
+    
+   console.log(" Welcome " + this.community + "! Cypher Source is glad to connect with you !");
   }
 }
+  var userdetails = new CySo( );
+// Object named "community" is declared globally. 
+  var community;
 
 // readline() method is used to get the user input dynamically .
-const readline = require('readline').createInterface({
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 readline.question('Hey buddy ! Your name please.. ?', name => {
-
-   console.log(" Welcome " + CySo.Welcome + "! Cypher Source is glad to connect with you !");
+  this.community = name;
+  userdetails.Welcome(this.community)
 
   readline.close();
 });
